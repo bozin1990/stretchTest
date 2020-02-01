af610dc490f5b68258037e69158fe675b0d776cb
bozin1990 <a0930832512@gmail.com>
2020-02-02 00:29:14 +0800

    Initial Commit

---
+++ stretchTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stretchTest
+//
+//  Created by 陳博軒 on 2020/2/2.
+//  Copyright © 2020 Bozin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
